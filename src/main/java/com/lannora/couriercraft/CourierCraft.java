45ddc7e53f24cb62929565856c1679e96d2906eb
Lannora <79697076+Lannora@users.noreply.github.com>
2025-08-17 10:16:42 +0200

    Ajout des items lettre et boite aux lettre

--- src/main/java/com/lannora/couriercraft/CourierCraft.java
+++ src/main/java/com/lannora/couriercraft/CourierCraft.java
@@ -1,5 +1,6 @@
 package com.lannora.couriercraft;
 
+import com.lannora.couriercraft.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class CourierCraft implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/lannora/couriercraft/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.lannora.couriercraft.item;
+
+import com.lannora.couriercraft.CourierCraft;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item LETTER = registerItem("letter", new Item(new Item.Settings()));
+    public static final Item MAILBOX = registerItem("mailbox", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(CourierCraft.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        CourierCraft.LOGGER.info(CourierCraft.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(LETTER);
+            fabricItemGroupEntries.add(MAILBOX);
+        });
+    }
+}
